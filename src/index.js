ce7cffcd1211e07ca8e64471eae3762e1a53eb17
pringshia <pratik@ringshia.com>
2018-02-19 21:21:05 -0500

    export some methods

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import Machinate from "./components/Machinate";
 import States from "./components/States";
+import { isTransitionable, createMachine } from "machinate";
 
-export { Machinate, States };
+export { Machinate, States, isTransitionable, createMachine };

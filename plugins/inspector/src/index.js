85f4444824c10530d8c0e23a914d238a55394dce
pringshia <pratik@ringshia.com>
2018-03-03 19:19:43 -0500

    Add Inspector plugin

---
+++ plugins/inspector/src/index.js
@@ -0,0 +1,268 @@
+import React from "react";
+import Frame from "react-frame-component";
+import PropTypes from "prop-types";
+import ReactDOM from "react-dom";
+import { transitionsTransform } from "./transforms";
+
+const INSTRUMENT = false;
+
+// const transitionsTransform = list => {
+//   const val = list.reduce(
+//     (info, item) => {
+//       if (info.last && item.toName === info.last.toName) {
+//         const lastItem = info.list[info.list.length - 1];
+//         lastItem.subItems = [...lastItem.subItems, item];
+//       } else {
+//         const newItem = { ...item, subItems: [] };
+//         info.list.push(newItem);
+//       }
+//       return { last: item, list: info.list };
+//     },
+//     { last: null, list: [] }
+//   );
+//   // console.log(val);
+//   return val;
+// };
+
+function bindEvent(element, eventName, eventHandler) {
+  if (element.addEventListener) {
+    element.addEventListener(eventName, eventHandler, false);
+  } else if (element.attachEvent) {
+    element.attachEvent("on" + eventName, eventHandler);
+  }
+}
+
+class InspectorState extends React.Component {
+  instrument = INSTRUMENT;
+  state = {
+    appState: null,
+    transitionsList: []
+  };
+  constructor(props) {
+    super(props);
+    this.state.appState = props.initial;
+  }
+  handleMessage = message => {
+    if (message.source === "machinate-to-inspector") {
+      this.instrument && console.log("received from parent: ", message);
+      if (message.type === "state") {
+        this.setState({ appState: message.data });
+      }
+      if (message.type === "transition") {
+        this.setState({
+          transitionsList: [...this.state.transitionsList, message.data]
+        });
+      }
+    }
+  };
+  render() {
+    return (
+      this.props.children &&
+      this.props.children({
+        ...this.state,
+        handleMessage: this.handleMessage.bind(this)
+      })
+    );
+  }
+}
+
+class MessageBroker extends React.Component {
+  postFromInspector = (type, data) => {
+    const window = this.window;
+
+    if (window) {
+      window.parent.postMessage(
+        { source: "machinate-from-inspector", type, data },
+        "*"
+      );
+    }
+  };
+
+  componentDidMount() {
+    this.window = this.context && this.context.window;
+
+    // this.postFromInspector("Hello!");
+
+    if (this.window) {
+      bindEvent(this.window, "message", e => {
+        this.props.onMessage(e.data);
+      });
+    }
+  }
+  render() {
+    return (
+      (this.props.children &&
+        this.props.children({ post: this.postFromInspector })) ||
+      null
+    );
+  }
+}
+MessageBroker.contextTypes = {
+  window: PropTypes.any,
+  document: PropTypes.any
+};
+
+class Inspector extends React.Component {
+  instrument = INSTRUMENT;
+  state = {};
+  postToInspector = (type, data) => {
+    const frame = ReactDOM.findDOMNode(this.iframe);
+    frame.contentWindow.postMessage(
+      { source: "machinate-to-inspector", type, data },
+      "*"
+    );
+  };
+  componentDidMount() {
+    const machine = this.context && this.context.machine;
+
+    if (window && machine) {
+      this.setState({ initialState: machine.getState() });
+      // console.log(machine.getState());
+      // this.postToInspector("init-state", machine.getState());
+
+      machine.addListener("transition", (type, data) =>
+        this.postToInspector("transition", data)
+      );
+      machine.addListener("set-state", (type, data) =>
+        this.postToInspector("state", data.state)
+      );
+      machine.addListener("force-state", (type, data) =>
+        this.postToInspector("state", data.state)
+      );
+
+      machine.addListener("log", (type, data) => console.log(data));
+
+      bindEvent(window, "message", e => {
+        if (e.data.source === "machinate-from-inspector") {
+          this.instrument && console.log("received from child", e.data);
+          if (e.data.type === "transition") {
+            machine.transition(
+              [],
+              ...e.data.data.name.split("."),
+              e.data.data.payload
+            );
+          } else if (e.data.type === "force-state") {
+            machine.setState(e.data.data);
+          }
+        }
+      });
+    }
+  }
+
+  render() {
+    return (
+      <div
+        style={{
+          position: "fixed",
+          top: "0",
+          right: "0",
+          height: "100vh",
+          width: "300px" // TODO: make resizable
+        }}
+      >
+        <Frame
+          ref={ref => {
+            if (ref) {
+              this.iframe = ref;
+            }
+          }}
+          style={{ border: "0", height: "100vh" }}
+          head={
+            <style>{`
+            * { box-sizing: border-box; }
+            html, body {margin:0; padding: 0; font-family: Arial, serif; }
+            body { background-color: #efefef; color: #3f3f3f; border-left: 1px solid #aaaaaa; min-height: 100vh; }
+            .container { padding: 20px 15px; font-size: 12px; }
+            html { height: 100vh; }
+            h1 { font-size: 14px; margin: 0; padding: 10px 15px; background-color: #d0d0d0; color: #333; border-bottom: 1px solid #777; }
+            h1, h3 { text-transform: uppercase; font-size: 11px; letter-spacing: 1px; }
+            h3 { margin: 0 0 5px 0; padding: 0 0 4px 0; border-bottom: 1px solid #d0d0d0; }
+            .module { margin-bottom: 30px; }
+            .transition { margin-bottom: 3px; }
+            .pointer { cursor: pointer; }
+            `}</style>
+          }
+        >
+          <InspectorState initial={this.state.initialState}>
+            {({ appState, transitionsList, handleMessage }) => (
+              <MessageBroker onMessage={handleMessage}>
+                {({ post }) => (
+                  <div>
+                    <h1 style={{ marginTop: "0px" }}>Inspector</h1>
+                    <div className="container">
+                      <div className="module">
+                        <h3>WATCH</h3>
+                      </div>
+                      <div className="module">
+                        <h3>STATE</h3>
+                        <div style={{ height: "200px", overflowY: "scroll" }}>
+                          {appState
+                            ? Object.keys(appState).map(domain => (
+                                <div key={domain}>
+                                  {domain}: {appState[domain].state}{" "}
+                                  {JSON.stringify(appState[domain].data)}
+                                </div>
+                              ))
+                            : "Undetected"}
+                        </div>
+                      </div>
+                      <div className="module">
+                        <h3>TRANSITIONS</h3>
+                        <div>
+                          {transitionsTransform(transitionsList).list.map(
+                            (t, idx) =>
+                              t.subItems.length >= 1 ? (
+                                <div className="transition" key={idx}>
+                                  {t.fromName} ➡ {t.toName}{" "}
+                                  {[t, ...t.subItems].map((sub, idx2) => (
+                                    <em
+                                      className="pointer"
+                                      key={idx + "-" + idx2}
+                                      onClick={(state => () =>
+                                        post("force-state", state))(sub.state)}
+                                    >
+                                      •{" "}
+                                    </em>
+                                  ))}
+                                </div>
+                              ) : (
+                                <div
+                                  className="transition pointer"
+                                  key={idx}
+                                  onClick={(state => () =>
+                                    post("force-state", state))(t.state)}
+                                >
+                                  {t.fromName} ➡ {t.toName}{" "}
+                                  <em>{t.payload ? "with payload" : ""}</em>
+                                </div>
+                              )
+                          )}
+                        </div>
+                      </div>
+                      {/* <button
+                    onClick={() =>
+                      post("transition", {
+                        name: "Auth.LoggedIn",
+                        payload: new Date()
+                      })
+                    }
+                  >
+                    Transition
+                  </button> */}
+                    </div>
+                  </div>
+                )}
+              </MessageBroker>
+            )}
+          </InspectorState>
+        </Frame>
+      </div>
+    );
+  }
+}
+
+Inspector.contextTypes = {
+  machine: PropTypes.object
+};
+
+export { Inspector };
---
+++ plugins/inspector/webpack.config.js
@@ -0,0 +1,20 @@
+const path = require("path");
+const isProd = process.env.NODE_ENV === "production";
+
+module.exports = {
+  entry: "./src/index.js",
+  devtool: isProd ? "cheap-module-source-map" : "source-map",
+
+  output: {
+    path: path.resolve("build"),
+    filename: "lib.js",
+    library: "MachinateInspector",
+    libraryTarget: "umd"
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: "babel-loader", exclude: /node_modules/ },
+      { test: /\.jsx$/, loader: "babel-loader", exclude: /node_modules/ }
+    ]
+  }
+};
